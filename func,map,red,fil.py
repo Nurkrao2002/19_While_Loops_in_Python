a0122efa7d879a243f3e7dbcf456f02c8a4228e9
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+Nurkrao2002@users.noreply.github.com>
2022-09-19 14:21:53 +0530

    Update func,map,red,fil.py

--- func,map,red,fil.py
+++ func,map,red,fil.py
@@ -1,38 +1,38 @@
 # a=["1","2","3","4","5"]
-# for i in range(len(a)):
-#     a[i]=int(a[i])
-#     print(a[i])
+for i in range(len(a)):
+    a[i]=int(a[i])
+    print(a[i])
 
-# v=["10","20","30","40"]
-# b=list(map(int,v))
-# print(b)
+v=["10","20","30","40"]
+b=list(map(int,v))
+print(b)
 
-# def sq(y):
-#     return y*y
-# n=[2,3,4,5,6]
-# s=list(map(sq,n))
-# g=list(map(lambda x: x*x,n))
-# print(s)
-# print(g)
+def sq(y):
+    return y*y
+n=[2,3,4,5,6]
+s=list(map(sq,n))
+g=list(map(lambda x: x*x,n))
+print(s)
+print(g)
 
-# def square(s): return s*s
-# def cube(c): return c*c*c
-# f=[square,cube]
-# for i in range(1,6):
-#     a=list(map(lambda x:x(i),f))
-#     print(a)
+def square(s): return s*s
+def cube(c): return c*c*c
+f=[square,cube]
+for i in range(1,6):
+    a=list(map(lambda x:x(i),f))
+    print(a)
 
-# a=[1,2,3,4,5,6,7,8,9,10]
-# def gr(n):
-#     return n>5
-# def ls(n):
-#     return n<5
-# v=list(filter(gr,a))
-# b=list(filter(ls,a))
-# print(v)
-# print(b)
+a=[1,2,3,4,5,6,7,8,9,10]
+def gr(n):
+    return n>5
+def ls(n):
+    return n<5
+v=list(filter(gr,a))
+b=list(filter(ls,a))
+print(v)
+print(b)
 
-# from functools import reduce
-# e=[5,10,15,20,25]
-# n=reduce(lambda x,y: x+y,e)
-# print(n)
+from functools import reduce
+e=[5,10,15,20,25]
+n=reduce(lambda x,y: x+y,e)
+print(n)
